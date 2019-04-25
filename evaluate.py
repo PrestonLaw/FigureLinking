25d289714be1112efddc430bea115695ad6f2544
PrestonLaw <preston.law@gmail.com>
2019-04-24 21:35:14 -0400

    Added directory to contain outputs of unit tests, cleaned up commented-out code

--- evaluate.py
+++ evaluate.py
@@ -125,46 +125,6 @@ def read_ann(filename):
                 ann_dict[e_id][a_type] = a_val
 
 
-                """
-                type = ""
-                num = 0
-                reftype = "Caption"
-                while line and line[0] != "T":
-                    if line[0] == "#":
-                        line = f.readline()
-                        continue
-                    # Now that I have an attribute line, parse it.
-                    tokens = line.split()
-
-                    if tokens[1] == "Num":
-                        num = int(tokens[3])
-                        #print("Adding character range for figure number %d."%num)
-                    elif tokens[1] == "Type":
-                        type = tokens[3]
-                        #print("Adding character range for figure with type '%s'."%type)
-                    elif tokens[1] == "RefType":
-                        reftype = tokens[3]
-
-                    line = f.readline()
-
-                # The attributes of the annotationg have been read in.  Store it directly.
-
-                annot_count += 1
-                #print("Annotation %d: %s, %s, %d"%(annot_count, reftype, type, num))
-
-                ann_dict = {}
-                ann_dict["range"] = char_range
-                ann_dict["EntityType"] = entitytype
-                ann_dict["RefType"] = reftype
-                ann_dict["Type"] = type
-                ann_dict["Num"] = num
-                ann_dict["Text"] = text
-
-                if config["Allowed REFTYPES"][reftype] != 'true' or config["Allowed TYPES"][type] != 'true':
-                    continue
-
-                ann_list.append(ann_dict)
-                """
 
             # Reading in the new line to continue the loop.
             line = f.readline()
@@ -209,17 +169,8 @@ def PR(overlap, gt, ex):
 
 def range_within_category(ann_list):
 
-    #total = 0
-
     range_totals = {ET: 0.0 for ET in EntityTypes}
     count_totals = {ET: 0 for ET in EntityTypes}
-
-    #cap_range_total = 0.0
-    #ref_range_total = 0.0
-    #cap_count_total = 0
-    #ref_count_total = 0
-
-    #pdb.set_trace()
 
     for i in range(len(ann_list)):
 
@@ -231,17 +182,6 @@ def range_within_category(ann_list):
         range_totals[ann_list[i]["EntityType"]] += len(ann_list[i]["range"])
         count_totals[ann_list[i]["EntityType"]] += 1
 
-        #if ann_list[i]["RefType"] in include_reftype and ann_list[i]["Type"] in include_type:
-        #if config["Allowed REFTYPES"][ann_list[i]["RefType"]] == "true" and \
-        #   config["Allowed TYPES"][ann_list[i]["Type"]] == "true":
-        #    if ann_list[i]["EntityType"] == "Caption":
-        #        cap_range_total += len(ann_list[i]["range"])
-        #        cap_count_total += 1
-        #    else:
-        #        ref_range_total += len(ann_list[i]["range"])
-        #        ref_count_total += 1
-
-    #return (cap_range_total, cap_count_total, ref_range_total, ref_count_total)
     return (range_totals, count_totals)
 
 
@@ -258,13 +198,13 @@ if __name__ == "__main__":
 
 
 
-    # Step 1: Parse the arguments.
+    # Part 1: Parse the arguments.
 
     args = parse_args()
 
 
 
-    # Step 2: Parse the config file.
+    # Part 2: Parse the config file.
 
     config = configparser.ConfigParser()
     config.optionxform=str
@@ -274,11 +214,7 @@ if __name__ == "__main__":
 
 
 
-    # Step 3: Parse the grountruth and non-groundtruth files into
-
-    # Part 3: Count how many Caption and Reference entities there are in the Groundtruth and NonGroundtruth files.
-
-    #pdb.set_trace()
+    # Part 3: Parse the annotations files and count how many Caption and Reference entities there are in the Groundtruth and NonGroundtruth files.
 
     list_gt = read_ann(args.gtfile)
     list_ex = read_ann(args.resfile)
@@ -288,36 +224,15 @@ if __name__ == "__main__":
         for ET in EntityTypes
     }
 
-    #entity_statistics = {
-    #    'gt_ref': 0,
-    #    'gt_cap': 0,
-    #    'ex_ref': 0,
-    #    'ex_cap': 0
-    #}
-
     for ann in list_gt:
         entity_statistics[ann["EntityType"]]["gt"] += 1
     for ann in list_ex:
         entity_statistics[ann["EntityType"]]["ex"] += 1
 
-    #for ann in list_gt:
-    #    if ann["EntityType"] == "Caption":
-    #        entity_statistics["gt_cap"] += 1
-    #    else:
-    #        entity_statistics["gt_ref"] += 1
-    #for ann in list_ex:
-    #    if ann["EntityType"] == "Caption":
-    #        entity_statistics["ex_cap"] += 1
-    #    else:
-    #        entity_statistics["ex_ref"] += 1
-
 
 
     # Part 4: Find both character length count and number of entities within the configuration parameters for
     # the four categories delineated by Caption versus Reference entitytype and Groundtruth versus NonGroundtruth file.
-
-    #cap_range_total_gt, cap_count_total_gt, ref_range_total_gt, ref_count_total_gt = range_within_category(list_gt)
-    #cap_range_total_ex, cap_count_total_ex, ref_range_total_ex, ref_count_total_ex = range_within_category(list_ex)
 
     gt_ranges, gt_counts = range_within_category(list_gt)
     ex_ranges, ex_counts = range_within_category(list_ex)
@@ -326,8 +241,6 @@ if __name__ == "__main__":
 
     # Part 5: Find all preliminary match pairs, pairs that match without regard to configuration parameters
     # and only by the minimum requirement of having at least one character overlap.
-
-    #pdb.set_trace()
 
     match_dict = {}
 
@@ -344,7 +257,6 @@ if __name__ == "__main__":
             overlap = len(set(ann_gt["range"]).intersection(set(ann_ex["range"])))
 
             if overlap > 0:
-                #pdb.set_trace()
 
                 f_key = (freeze(ann_gt), freeze(ann_ex))
 
@@ -365,27 +277,9 @@ if __name__ == "__main__":
         } for ET in EntityTypes
     }
 
-    #match_counts = {
-    #    'Caption': {
-    #        "TP": 0,
-    #        "FN": 0,
-    #        "FP": 0
-    #    },
-    #    'Reference': {
-    #        "TP": 0,
-    #        "FN": 0,
-    #        "FP": 0
-    #    }
-    #}
-
     match_overlaps = {
         ET: 0.0 for ET in EntityTypes
     }
-
-    #match_overlaps = {
-    #    "Caption": 0.0,
-    #    "Reference": 0.0
-    #}
 
     reqs = config["Match Requirements"]
 
@@ -395,14 +289,6 @@ if __name__ == "__main__":
     T_entities = []
     F_entities = []
     M_entities = []
-
-    #matched_entities = []
-
-    #TP_matches = []
-
-    #TP_match_pairs = []
-    #FP_match_pairs = []
-    #FN_matches = []
 
 
 
@@ -437,11 +323,6 @@ if __name__ == "__main__":
             F_matches.append((f_gt, f_ex))
             match_counts[gt["EntityType"]]["FM"] += 1
 
-            #FP_match_pairs.append((f_gt, f_ex))
-            #if gt["EntityType"] == "Caption":
-            #    match_counts["Caption"]["FP"] += 1
-            #else:
-            #    match_counts["Reference"]["FP"] += 1
             continue
 
 
@@ -453,20 +334,9 @@ if __name__ == "__main__":
         match_counts[gt["EntityType"]]["TM"] += 1
         match_overlaps[gt["EntityType"]] += overlap
 
-        #if gt["EntityType"] == "Caption":
-        #    match_counts["Caption"]["TP"] += 1
-        #    match_overlaps["Caption"] += overlap
-        #else:
-        #    match_counts["Reference"]["TP"] += 1
-        #    match_overlaps["Reference"] += overlap
-
 
 
         # Part 7.3: We need to keep track of which GT entities are true matched outside of the loop.
-
-        #T_entities.append(f_gt)
-        #T_entities.append(f_ex)
-        #T_matches.append((f_gt, f_ex))
 
 
 
@@ -489,7 +359,7 @@ if __name__ == "__main__":
 
 
 
-    # Step _: Print the output.
+    # Step 9: Print the output.
 
     with open(args.evalfile, "w") as of:
 
@@ -511,7 +381,6 @@ if __name__ == "__main__":
         rwrite(of, "-"*50)
         rwrite(of, "CONFIGURATION PARAMETERS\n")
 
-        #rwrite(of, "Span Overlap Threshold: %d
         rwrite(of, "Span Overlap Threshold: %s"%config["Match Requirements"]["Overlap Threshold"])
         rwrite(of, "REFTYPE Categories Tested:")
         for key in config["Allowed REFTYPES"].keys():
@@ -528,18 +397,15 @@ if __name__ == "__main__":
 
         # Part 9.3: The number of Caption and Reference entities in each of the GT and non-GT files.
 
-        rwrite(of, "ENTITY STATISTICS")
+        rwrite(of, "-"*50)
+        rwrite(of, "ENTITY STATISTICS\n")
         rwrite(of, "Ground Truth")
         for ET in EntityTypes:
             rwrite(of, "    # of '%s': %d"%(ET, entity_statistics[ET]["gt"]))
 
-        #rwrite(of, "    # of 'Caption':   %d"%(entity_statistics['gt_cap']))
-        #rwrite(of, "    # of 'Reference': %d"%(entity_statistics['gt_ref']))
         rwrite(of, "Results")
         for ET in EntityTypes:
              rwrite(of, "    # of '%s': %d"%(ET, entity_statistics[ET]["ex"]))
-        #rwrite(of, "    # of 'Caption':   %d"%(entity_statistics['ex_cap']))
-        #rwrite(of, "    # of 'Reference': %d\n"%(entity_statistics['ex_ref']))
 
         rwrite(of, "\n" + "-" * 50)
 
@@ -547,7 +413,7 @@ if __name__ == "__main__":
 
         # Part 9.4: The number of true positive, false positive, and false negative matches on the GT entities.
 
-        rwrite(of, "EVALUATION RESULTS")
+        rwrite(of, "EVALUATION RESULTS\n")
         rwrite(of, "Match Counts")
         rwrite(of, "    Overall (all ENTITYTYPES):")
         rwrite(of, "        # of True Matches:    %d"%(sum([match_counts[ET]["TM"] for ET in EntityTypes])))
@@ -560,15 +426,6 @@ if __name__ == "__main__":
             rwrite(of, "        # of False Matches:   %d"%(match_counts[ET]["FM"]))
             rwrite(of, "        # of False Entities:  %d"%(match_counts[ET]["FE"]))
             rwrite(of, "        # of Missed Entities: %d"%(match_counts[ET]["ME"]))
-
-        #rwrite(of, "    'Caption' ENTITYTYPE Only:")
-        #rwrite(of, "        # of Correct Matches:        %d"%(match_counts["Caption"]["TP"]))
-        #rwrite(of, "        # of False Negative Matches: %d"%(match_counts["Caption"]["FN"]))
-        #rwrite(of, "        # of False Positive Matches: %d"%(match_counts["Caption"]["FP"]))
-        #rwrite(of, "    'Reference' ENTITYTYPE Only:")
-        #rwrite(of, "        # of Correct Matches:        %d"%(match_counts["Reference"]["TP"]))
-        #rwrite(of, "        # of False Negative Matches: %d"%(match_counts["Reference"]["FN"]))
-        #rwrite(of, "        # of False Positive Matches: %d"%(match_counts["Reference"]["FP"]))
 
 
 
@@ -584,15 +441,9 @@ if __name__ == "__main__":
         rwrite(of, "    Overall (all ENTITYTYPES):")
         rwrite(of, "        Overlap (computed on overlap between text spans):")
         P,R = PR(sum(match_overlaps.values()), sum(gt_ranges.values()), sum(ex_ranges.values()))
-        #P,R = PR(match_overlaps["Caption"] + match_overlaps["Reference"],
-        #         cap_range_total_gt + ref_range_total_gt,
-        #         cap_range_total_ex + ref_range_total_ex)
         rwrite(of, "            Precision = %f, Recall = %f"%(P,R))
         rwrite(of, "        Count (computed on # of correct/incorrect matches):")
         P,R = PR(sum([match_counts[ET]["TM"] for ET in EntityTypes]), sum(gt_counts.values()), sum(ex_counts.values()))
-        #P,R = PR(match_counts["Caption"]["TP"] + match_counts["Reference"]["TP"],
-        #         cap_count_total_gt + ref_count_total_gt,
-        #         cap_count_total_ex + ref_count_total_ex)
         rwrite(of, "            Precision = %f, Recall = %f"%(P,R))
 
 
@@ -610,32 +461,13 @@ if __name__ == "__main__":
             P,R = PR(match_counts[ET]["TM"], gt_counts[ET], ex_counts[ET])
             rwrite(of, "            Precision = %f, Recall = %f"%(P,R))
 
-        """
-        rwrite(of, "    'Caption' ENTITYTYPE Only:")
-        rwrite(of, "        Overlap (computed on overlap between text spans):")
-        P,R = PR(match_overlaps["Caption"], cap_range_total_gt, cap_range_total_ex)
-        rwrite(of, "            Precision = %f, Recall = %f"%(P,R))
-        rwrite(of, "        Count (computed on # of correct/incorrect matches):")
-        P,R = PR(match_counts["Caption"]["TP"], cap_count_total_gt, cap_count_total_ex)
-        rwrite(of, "            Precision = %f, Recall = %f"%(P,R))
-
-        rwrite(of, "    'Reference' ENTITYTYPE Only:")
-        rwrite(of, "        Overlap (computed on overlap between text spans):")
-        P,R = PR(match_overlaps["Reference"], ref_range_total_gt, ref_range_total_ex)
-        rwrite(of, "            Precision = %f, Recall = %f"%(P,R))
-        rwrite(of, "        Count (computed on # of correct/incorrect matches):")
-        P,R = PR(match_counts["Reference"]["TP"], ref_count_total_gt, ref_count_total_ex)
-        rwrite(of, "            Precision = %f, Recall = %f"%(P,R))
-        """
-
         rwrite(of, "\n" + "-" * 50)
 
 
 
         # Part 9.6: Print out all true matches, false matches, false entities, and missed entities.
-        rwrite(of, "")
         rwrite(of, "Match Contents")
-        rwrite(of, "TRUE MATCHES")
+        rwrite(of, "\nTRUE MATCHES")
         count = 0
         for (f_gt, f_ex) in T_matches:
 
@@ -654,13 +486,6 @@ if __name__ == "__main__":
             rwrite(of, "    R: %s %s [%d %d] %s %s %d"%(ex["ID"], ex["EntityType"], min(ex["range"]), max(ex["range"]), ex["RefType"], ex["Type"], ex["Num"]))
             rwrite(of, "    G: %s %s [%d %d] %s %s %d"%(gt["ID"], gt["EntityType"], min(gt["range"]), max(gt["range"]), gt["RefType"], gt["Type"], gt["Num"]))
 
-            #rwrite(of, "    ENTITYTYPES: %s, %s"%(gt["EntityType"], ex["EntityType"]))
-            #rwrite(of, "    REFTYPES: %s, %s"%(gt["RefType"], ex["RefType"]))
-            #rwrite(of, "    TYPES: %s, %s"%(gt["Type"], ex["Type"]))
-            #rwrite(of, "    NUMS: %d, %d"%(gt["Num"], ex["Num"]))
-            #rwrite(of, "    Text of GT / non-GT entities:")
-            #rwrite(of, "%s"%(gt["Text"]))
-            #rwrite(of, "%s\n"%(ex["Text"]))
 
 
         rwrite(of, "\nFALSE MATCHES")
@@ -683,13 +508,7 @@ if __name__ == "__main__":
             rwrite(of, "    R: %s %s [%d %d] %s %s %d"%(ex["ID"], ex["EntityType"], min(ex["range"]), max(ex["range"]), ex["RefType"], ex["Type"], ex["Num"]))
             rwrite(of, "    G: %s %s [%d %d] %s %s %d"%(gt["ID"], gt["EntityType"], min(gt["range"]), max(gt["range"]), gt["RefType"], gt["Type"], gt["Num"]))
 
-            #rwrite(of, "    ENTITYTYPES: %s, %s"%(gt["EntityType"], ex["EntityType"]))
-            #rwrite(of, "    REFTYPES: %s, %s"%(gt["RefType"], ex["RefType"]))
-            #rwrite(of, "    TYPES: %s, %s"%(gt["Type"], ex["Type"]))
-            #rwrite(of, "    NUMS: %d, %d"%(gt["Num"], ex["Num"]))
-            #rwrite(of, "    Text of GT / non-GT entities:")
-            #rwrite(of, "%s"%(gt["Text"]))
-            #rwrite(of, "%s\n"%(ex["Text"]))
+
 
         rwrite(of, "\nFALSE ENTITIES")
         count = 0
@@ -700,6 +519,8 @@ if __name__ == "__main__":
             count += 1
             rwrite(of, "Entity %d:"%(count))
             rwrite(of, "    R: %s %s [%d %d] %s %s %d"%(ex["ID"], ex["EntityType"], min(ex["range"]), max(ex["range"]), ex["RefType"], ex["Type"], ex["Num"]))
+
+
 
         rwrite(of, "\nMISSED ENTITIES")
         count = 0
